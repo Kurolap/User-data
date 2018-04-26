aafdc2e8902f49eb72ba962fc5a2e3e72bb34d14
mkurolap <mkurolap@gmail.com>
2018-04-26 23:36:42 +0300

    server installed

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: 'public' });
+});
+var server = app.listen(750, function () {
+ console.log("Server started on 'http://localhost:750'");
+});
