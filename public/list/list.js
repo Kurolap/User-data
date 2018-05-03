48d887853489bcee4a430c44e595c065677131ab
mkurolap <mkurolap@gmail.com>
2018-05-04 00:19:30 +0300

    List.js added

---
+++ public/list/list.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var data = [
+        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "Akulov@mail.com"},
+        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "Akulov@mail.com"},
+        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "Akulov@mail.com"},
+    ];
+});
