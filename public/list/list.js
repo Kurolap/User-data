f9ca3ed575ec304d32606c75d436ffe69f0967dd
mkurolap <mkurolap@gmail.com>
2018-05-08 02:08:13 +0300

    Glyphicons changed

--- public/list/list.js
+++ public/list/list.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var data = [
-        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "Akulov@mail.com"},
-        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "Akulov@mail.com"},
-        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "Akulov@mail.com"},
+        {first_name: "Sergey", second_name: "Akulov", gender: "male", age: 29, email: "akulov@mail.com"},
+        {first_name: "Ivan", second_name: "Petrov", gender: "male", age: 28, email: "petrov@mail.com"},
+        {first_name: "Elena", second_name: "Sidorova", gender: "female", age: 25, email: "sidorova@mail.com"},
     ];
 });
