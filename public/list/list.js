250c3fc3a460ba58e8fb03078ac1bc1dbf4f39e7
mkurolap <mkurolap@gmail.com>
2018-05-22 23:06:19 +0300

    Show/Hide functionality has been implemented

--- public/list/list.js
+++ public/list/list.js
@@ -35,5 +35,24 @@ $(document).ready(function() {
     $('#add').click(function(event) {
         $('#form').show();
         $('#list').hide();
+        var newTxt = '';
+        $("#fname").val(newTxt);
+        $("#sname").val(newTxt);
+        $("#email").val(newTxt);
+        $("#pwd").val(newTxt);
+        $("#gender").val(newTxt);
+        $("#date").val(newTxt);
+    });
+    $('#cancel-btn').click(function(event) {
+        $('#form').hide();
+        $('#list').show();
+    });
+    $('#save-btn').click(function(event) {
+        $('#form').hide();
+        $('#list').show();
+    });
+    $('.glyphicon-pencil').click(function(event) {
+        $('#form').show();
+        $('#list').hide();
     });
 });
